f96c0ee14e2977bd32aa8b6e5571390ca7198d57
yannik <yannik.soyka@icloud.com>
2023-10-14 08:52:49 +0200

    2023-10-14

---
+++ Java/WIllkomen.java/willkommen.java
@@ -0,0 +1,6 @@
+public class Willkommen {
+    
+    public static void main(String[] args) {
+        System.out.print("Herzlich Willkommen zum Java-Kurs!");
+    }
+}
---
+++ Java/hello.java
@@ -0,0 +1,9 @@
+
+class Willkommen {
+    
+    public static void main(String[] args) {
+        System.out.println("Herzlich Willkommen zum Java-Kurs!");
+    }
+}   
+
+
